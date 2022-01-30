fbfa9e70f8e03ea5f48702d8d8e18751e7d1205e
Qwers1221 <stialerminers@gmail.com>
2022-01-31 00:11:51 +0200

    First Load Project!

---
+++ UFO/Assets/Scripts/MenuMain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BtnDonate()
+    {
+        Application.OpenURL("https://www.youtube.com/");
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+    public void BtnPlay()
+    {
+        SceneManager.LoadScene("game");
+    }
+    public void BtnSettings()
+    {
+
+    }
+}
